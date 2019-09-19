f34959709a3dcea8e07c07898ee1a91eca51256f
Kirill Maksimov <kirill@orbs.com>
2019-09-19 15:34:55 +0300

    Add README, refactor test to look nicer

---
+++ README.md
@@ -0,0 +1,13 @@
+# Wallet Beta
+
+Partial implementation of the [Wallet API](https://github.com/orbs-network/orbs-client-sdk-javascript/issues/20), currently only works in Firefox.
+
+To install the extention, use `about:debugging#/runtime/this-firefox` page in Firefox.
+
+## Building
+
+`npm run build`
+
+## Running the tests
+
+`open ./firefox/test/test.html`
--- firefox/test/test.js
+++ firefox/test/test.js
@@ -1,24 +1,30 @@
 import { Client, NetworkType, encodeHex, argAddress, argString } from "orbs-client-sdk";
 import { Wallet } from "../../src/wallet/wallet";
 
-async function test(accounts, accountId) {
+async function test(accounts, accountId, name, log) {
     const account = accounts[accountId];
     const addr = await account.getAddress();
-    console.log(`Account ${accountId} address`, addr);
+    log(`[Account ${accountId}] address`, addr);
 
     const pubKey = await account.getPublicKey();
-    console.log(`Account ${accountId} public key`, encodeHex(pubKey));
+    log(`[Account ${accountId}] public key`, encodeHex(pubKey));
     const signature0 = await account.signEd25519(pubKey);
-    console.log(`Account ${accountId} signed payload`, encodeHex(signature0));
+    log(`[Account ${accountId}] signed payload`, encodeHex(signature0));
 
     const client = new Client("https://node1.demonet.orbs.com/vchains/1000", 1000, NetworkType.NETWORK_TYPE_TEST_NET, account);
     const query = await client.createQuery("NamesV5", "get", [argAddress(addr)]);
     const result = await client.sendQuery(query);
-    console.log(`Account ${accountId} name (from NamesV5 contract on Demonet) is`, result.outputArguments[0].value);
+    log(`[Account ${accountId}] name (from NamesV5 contract on Demonet) is`, result.outputArguments[0].value);
 
-    const [ tx, txId ] = await client.createTransaction("NamesV5", "set", [argString("Kirill")]);
+    const [ tx, txId ] = await client.createTransaction("NamesV5", "set", [argString(name)]);
     const txResult = await client.sendTransaction(tx);
-    console.log(`Updating account ${accountId} name status`, txResult.executionResult);
+    log(`[Account ${accountId}] update name`, txResult.executionResult);
+}
+
+function log(...contents) {
+    const p = document.createElement("p");
+    p.textContent = contents.join(" ");
+    document.body.appendChild(p);
 }
 
 (async () => {
@@ -30,7 +36,7 @@ async function test(accounts, accountId) {
     const wallet = new Wallet(window);
     const accounts = await wallet.enable();
 
-    await test(accounts, 0);
-    await test(accounts, 1);
-    await test(accounts, 2);
+    await test(accounts, 0, "Iggy Pop", log);
+    await test(accounts, 1, "David Bowie", log);
+    await test(accounts, 2, "Avril Lavigne", log);
 })();
