299e24ccd145b13cc7c1940ea1213f35ebb6b70d
Kirill Maksimov <kirill@orbs.com>
2019-09-19 13:20:55 +0300

    Full working e2e

--- firefox/test/test.js
+++ firefox/test/test.js
@@ -1,4 +1,4 @@
-import { encodeHex, decodeHex } from "orbs-client-sdk";
+import { encodeHex, decodeHex, Client, argAddress, NetworkType } from "orbs-client-sdk";
 
 class ExtensionProxy {
     constructor(context) {
@@ -79,4 +79,9 @@ class Wallet {
     console.log("Account 0 public key", encodeHex(pubKey0));
     const signature0 = await accounts[0].signEd25519(pubKey0);
     console.log("Account 0 signed payload", encodeHex(signature0));
+
+    const client = new Client("https://node1.demonet.orbs.com/vchains/1000", 1000, NetworkType.NETWORK_TYPE_TEST_NET, accounts[0]);
+    const query = await client.createQuery("NamesV5", "get", [argAddress("0x99eb4af1f3b3b8619c7e3d99dc5a13c966f9d6e2")]);
+    const result = await client.sendQuery(query);
+    console.log(result.outputArguments[0]);
 })();
--- src/export.js
+++ src/export.js
@@ -1,28 +1,56 @@
-import { createAccount, DefaultSigner, Account, encodeHex } from "orbs-client-sdk";
+import { createAccount, DefaultSigner, encodeHex, decodeHex } from "orbs-client-sdk";
 import { get } from "lodash";
+
+class ExtensionSigner extends DefaultSigner {
+    constructor(account) {
+        super(account);
+        this.fields.address = account.address;
+    }
+
+    async getAddress() {
+        return this.fields.address;
+    }
+}
+
+function getAccount(id) {
+    const PUBLIC_KEY = `sender_public_key_${id}`;
+    const PRIVATE_KEY = `sender_private_key_${id}`;
+    const ADDRESS = `sender_address_${id}`;
+
+    let account;
+    if (!localStorage.getItem(PUBLIC_KEY)) {
+        account = createAccount();
+        localStorage.setItem(PUBLIC_KEY, encodeHex(account.publicKey));
+        localStorage.setItem(PRIVATE_KEY, encodeHex(account.privateKey));
+        localStorage.setItem(ADDRESS, account.address);
+    } else {
+        account = {
+            publicKey: decodeHex(localStorage.getItem(PUBLIC_KEY)),
+            privateKey: decodeHex(localStorage.getItem(PRIVATE_KEY)),
+            address: localStorage.getItem(ADDRESS)
+        }
+    }
+
+    return new ExtensionSigner(account);
+}
 
 export class Wallet {
     constructor() {
-        this.accounts = [new DefaultSigner(createAccount())]
+        this.accounts = [getAccount(0), getAccount(1), getAccount(2)];
     }
 
     async getAccounts() {
-        return this.accounts
+        return this.accounts;
     }
 }
 
 const wallet = new Wallet();
 
-function onMessage({ type, method, params, callbackName }) {
+async function onMessage({ type, method, params, callbackName }) {
     if (type == "call") {
         try {
-            // wallet.accounts[0][method](...params).then(value => {
-            //     window.wrappedJSObject[callbackName]("response", value);
-            // }).catch(console.error)
-
-            const value = wallet.accounts[0][method](...params);
-            // console.log("val", value)
-
+            // console.log(method, params, callbackName)
+            const value = await wallet.accounts[0][method](...params);
             let serializedValue = value;
             let type;
             if (get(value, "__proto__.constructor.name") == "Uint8Array") {
@@ -34,7 +62,6 @@ function onMessage({ type, method, params, callbackName }) {
             console.log("Failed to call page code", e);
         }
     }
-    
 }
 
 exportFunction(onMessage, window, {defineAs: "orbsWalletSendMessage"});
